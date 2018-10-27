de32e13db51a0c2d733e6dabf8fad108fb9e46b1
Kenshin Abe <abe.kenshin@gmail.com>
2018-10-27 16:24:36 +0900

    Implement upper bound for BIT

--- BinaryIndexedTree.cpp
+++ BinaryIndexedTree.cpp
@@ -21,28 +21,24 @@ template <class T> ostream& operator<<(ostream &s, const vector<T>& d) { int n =
 template <class T> ostream& operator<<(ostream &s, const vector<vector<T>>& dd) { for (vector<T> d: dd) s << d << endl; return s; }
 struct Fast { Fast() { cin.tie(0); ios::sync_with_stdio(false); } } fast;
 const int MOD = 1e9 + 7;
-
 // 0-indexed
 class BinaryIndexedTree {
   int max_pow; // N以下の最大の2べき
-public:
-  int N;
-  VI bit;
-  BinaryIndexedTree(int n) : N(n), bit(n) {
-    for (max_pow = 1; max_pow <= N / 2; max_pow *= 2);
-  }
-  BinaryIndexedTree(const VI& a) : N(a.size()), bit(a) {
+  void init() {
     for (max_pow = 1; max_pow <= N / 2; max_pow *= 2);
     REP (i, N - 1) if ((i | (i + 1)) < N) {
       bit[i | (i + 1)] += bit[i];
     }
   }
+public:
+  VI bit; int N;
+  BinaryIndexedTree(int n, int x = 0) : N(n), bit(n, x) { init(); }
+  BinaryIndexedTree(const VI& a) : N(a.size()), bit(a) { init(); }
   // k番目にxを加算
   void add(int k, int x) {
-    for (int i = k; i < N; i |= i + 1) {
-      bit[i] += x;
-    }
+    for (int i = k; i < N; i |= i + 1) { bit[i] += x; }
   }
+
   // [0, k)、k == 0のときは0を返す
   int sum(int k) {
     int ret = 0;
@@ -51,21 +47,24 @@ public:
     }
     return ret;
   }
+  int sum() { return sum(N); }
+
   // [a, b)
   int query(int a, int b) {
-    assert(a < b);
+    assert(a <= b);
     return sum(b) - sum(a);
   }
   int operator[](int k) {
     return query(k, k + 1);
   }
+
   // k番目をxに
   void update(int k, int x) {
-    add(k, x - query(k, k + 1));
+    add(k, x - operator[](k));
   }
 
-  // sum(i) >= xとなるような最小のi
   // 注: 中身が全て正のときしか動かない
+  // sum(i + 1) >= xとなるような最小のi
   int lower_bound(int x) {
     int i = -1;
     for (int w = max_pow; w > 0; w /= 2) {
@@ -76,7 +75,29 @@ public:
     }
     return i + 1;
   }
+  // query[begin, i + 1) >= xとなるような最小のi
+  int lower_bound(int begin, int x) {
+    int lb = lower_bound(x + sum(begin));
+    return max(begin, lb);
+  }
+  // sum(i + 1) > xとなるような最小のi
+  int upper_bound(int x) {
+    int i = -1;
+    for (int w = max_pow; w > 0; w /= 2) {
+      if (i + w < N && bit[i + w] <= x) {
+        x -= bit[i + w];
+        i += w;
+      }
+    }
+    return i + 1;
+  }
+  // query[begin, i + 1) > xとなるような最小のi
+  int upper_bound(int begin, int x) {
+    int ub = upper_bound(x + sum(begin));
+    return max(begin, ub);
+  }
 };
 
 signed main() {
 }
+
