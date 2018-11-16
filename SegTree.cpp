ffc804b2296d5ef7db37d5e8ef165d0440178191
Kenshin Abe <abe.kenshin@gmail.com>
2018-11-16 16:41:02 +0900

    Fix SegTree

--- SegTree.cpp
+++ SegTree.cpp
@@ -1,14 +1,28 @@
 #include <bits/stdc++.h>
 #define ll long long
-#define REP(i, n) for (ll (i) = 0; (i) < (n); (i)++)
-#define REPI(i, a, b) for (ll (i) = (a); (i) < (b); (i)++)
+#define REP(i, n) for (ll i = 0, max_i = (n); i < max_i; i++)
+#define REPI(i, a, b) for (ll i = (a), max_i = (b); i < max_i; i++)
+#define ALL(obj) (obj).begin(), (obj).end()
+#define RALL(obj) (obj).rbegin(), (obj).rend()
+#define fi first
+#define se second
+#define pb push_back
+#define debug(x) cerr << #x << ": " << (x) << endl
+#define debug2(x, y) cerr << #x << ": " << (x) << " " << #y << ": " << y << endl;
 #define int long long
 using namespace std;
 using II = pair<int, int>;
+using VII = vector<II>;
 using VI = vector<int>;
 using VVI = vector<VI>;
 using VVVI = vector<VVI>;
-using VII = vector<II>;
+template <class T = int> inline T in() { T x; cin >> x; return x; }
+template <class T = int> inline bool chmax(T &a, const T &b) { if (a < b) { a = b; return true; } return false; }
+template <class T = int> inline bool chmin(T &a, const T &b) { if (a > b) { a = b; return true; } return false; }
+template <class T> ostream& operator<<(ostream &s, const vector<T>& d) { int n = d.size(); REP (i, n) s << d[i] << " "; return s; }
+template <class T> ostream& operator<<(ostream &s, const vector<vector<T>>& dd) { for (vector<T> d: dd) s << d << endl; return s; }
+struct Fast { Fast() { cin.tie(0); ios::sync_with_stdio(false); } } fast;
+const int MOD = 1e9 + 7;
 
 template <class T = int>
 class SegTree {
@@ -26,18 +40,14 @@ public:
   int N;
   VT dat;
   function<T(T, T)> f;
-  int UNIT;
-  SegTree(int n, function<T(T, T)> f_, const T unit) {
-    orig_n = n;
-    f = f_;
-    UNIT = unit;
+  T UNIT;
+  SegTree() {}
+  SegTree(int n, function<T(T, T)> f_, const T unit) : orig_n(n), f(f_), UNIT(unit) {
     for (N = 1; N < n; N *= 2);
     dat = VT(2 * N - 1, UNIT);
   }
-  SegTree(VT a = {}, function<T(T, T)> f_ = [](int a, int b) { return min(a, b); }, T unit = 1e15) {
-    orig_n = a.size();
-    f = f_;
-    UNIT = unit;
+  SegTree(const VT& a, function<T(T, T)> f_ = [](int a, int b) { return min(a, b); }, T unit = 1e15)
+  : orig_n(a.size()), f(f_), UNIT(unit) {
     for (N = 1; N < a.size(); N *= 2);
     dat = VT(2 * N - 1);
     REP (i, a.size()) {
