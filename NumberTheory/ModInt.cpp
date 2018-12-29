88dea0eb5d10f005caad028ec321675c59b44cb8
Kenshin Abe <abe.kenshin@gmail.com>
2018-12-29 16:57:43 +0900

    Update ModInt

--- NumberTheory/ModInt.cpp
+++ NumberTheory/ModInt.cpp
@@ -24,31 +24,35 @@ template <class T> ostream& operator<<(ostream &s, const vector<vector<T>>& dd) 
 struct Fast { Fast() { cin.tie(0); ios::sync_with_stdio(false); } } fast;
 const int MOD = 1e9 + 7;
 
-template <int MOD = MOD>
+template <int MD>
 struct ModInt {
   int x;
   ModInt() : x(0) {}
-  ModInt(int x_) { if ((x = x_ % MOD + MOD) >= MOD) x -= MOD; }
-  int get() const { return x; }
+  ModInt(int x_) { if ((x = x_ % MD + MD) >= MD) x -= MD; }
 
-  ModInt& operator+=(ModInt that) { if ((x += that.x) >= MOD) x -= MOD; return *this; }
-  ModInt& operator*=(ModInt that) { x = (unsigned long long)x * that.x % MOD; return *this; }
-  ModInt& operator-=(ModInt that) { if ((x -= that.x) <= MOD) x += MOD; return *this; }
+  ModInt& operator+=(ModInt that) { if ((x += that.x) >= MD) x -= MD; return *this; }
+  ModInt& operator*=(ModInt that) { x = (unsigned long long)x * that.x % MD; return *this; }
+  ModInt& operator-=(ModInt that) { if ((x -= that.x) <= MD) x += MD; return *this; }
+  ModInt& operator/=(ModInt that) { x = (unsigned long long)x * that.inv().x % MD; return *this; }
 
-  ModInt operator-() const { return -x < 0 ? MOD - x : -x; }
+  ModInt operator-() const { return -x < 0 ? MD - x : -x; }
   ModInt operator+(ModInt that) const { return ModInt(*this) += that; }
   ModInt operator*(ModInt that) const { return ModInt(*this) *= that; }
   ModInt operator-(ModInt that) const { return ModInt(*this) -= that; }
+  ModInt operator/(ModInt that) const { return ModInt(*this) /= that; }
+  ModInt pow(int n) const {
+    ModInt x = *this, r = 1;
+    while (n) {
+      if (n & 1) r *= x;
+      x *= x;
+      n >>= 1;
+    }
+    return r;
+  }
+  ModInt inv() const { return (*this).pow(MD - 2); }
+  friend ostream& operator<<(ostream& s, ModInt<MD> a) { s << a.x; return s; }
 };
-template <int MOD> ostream& operator<<(ostream &s, const ModInt<MOD> a) { s << a.x; return s; }
-using mint = ModInt<>;
-
-template <class T = int>
-T pow(T a, int x) {
-  if (x == 0) return 1;
-  T tmp = pow(a, x / 2);
-  return tmp * tmp * (x % 2 ? a : 1);
-}
+using mint = ModInt<MOD>;
 
 signed main() {
 }
