27e54275782f83f6390debcda7530da2cbe99edb
Kenshin Abe <abe.kenshin@gmail.com>
2019-08-12 03:33:31 +0900

    Imporove Rolling Hash

--- String/RollingHash.cpp
+++ String/RollingHash.cpp
@@ -1,47 +1,9 @@
 using ull = unsigned long long;
-random_device rnd;
-mt19937 mt(rnd());
-mt19937_64 mt64(rnd());
-int mod_pow(int x, int n, int mod) {
-    if (n <= 0) return 1;
-    int tmp = mod_pow(x, n / 2, mod);
-    return tmp * tmp % mod * (n % 2 ? x : 1) % mod;
-}
-// Miller-Rabin
-bool is_prime(int n, int times = 50) {
-    if (n == 2) return true;
-    if (n % 2 == 0 || n < 2) return false;
-    int d = n - 1;
-    while (d % 2 == 0) d /= 2;
-    while (times--) {
-        int a = rnd() % (n - 2) + 1;
-        int t = d;
-        int y = mod_pow(a, t, n);
-        while (t != n - 1 && y != 1 && y != n - 1) {
-            y = y * y % n;
-            t *= 2;
-        }
-        if (y != n - 1 && t % 2 == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-int gen_prime() {
-    int cnt = 0;
-    while (1) {
-        int n = mt();
-        n != 1;
-        if (is_prime(n)) return n;
-    }
-}
-class RollingHash {
-public:
+struct RollingHash {
     vector<ull> hash, pows;
-    ull mod;
-    RollingHash(const string& S, ull base = gen_prime(), ull m = gen_prime())
-        : hash(S.size() + 1), pows(S.size() + 1), mod(m) {
-        pows[0] = 1;
+    ull base, mod;
+    RollingHash(const string &S, ull base, ull mod = 1000000009)
+        : hash(S.size() + 1), pows(S.size() + 1, 1), mod(mod), base(base) {
         for (int i = 0; i < S.size(); i++) {
             pows[i + 1] = pows[i] * base % mod;
             hash[i + 1] = hash[i] * base % mod + S[i];
@@ -55,12 +17,42 @@ public:
         if (ret >= mod) ret -= mod;
         return ret;
     }
+    void concat(const string &T) {
+        int n = hash.size() - 1, m = T.size();
+        pows.resize(n + m + 1);
+        hash.resize(n + m + 1);
+        for (int i = 0; i < m; i++) {
+            pows[n + i + 1] = pows[n + i] * base % mod;
+            hash[n + i + 1] = hash[n + i] * base % mod + T[i];
+            if (hash[n + i + 1] >= mod) hash[n + i + 1] -= mod;
+        }
+    }
 };
-using UU = pair<ull, ull>;
-class DoubleRollingHash {
-public:
-    RollingHash rh1, rh2;
-    DoubleRollingHash(const string& S) : rh1(S), rh2(S) {}
-    // [l, r)
-    UU get(int l, int r) { return {rh1.get(l, r), rh2.get(l, r)}; }
+
+constexpr int HASH_NUM = 4;
+struct bases_t {
+    int use[HASH_NUM];
+    int &operator[](int i) { return use[i]; }
+    bases_t() {
+        mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+        for (int i = 0; i < HASH_NUM; i++) use[i] = rnd() % 10000;
+    }
+} bases;
+
+using multihash_t = array<int, HASH_NUM>;
+struct MultiRollingHash {
+    vector<RollingHash> rhs;
+    MultiRollingHash(const string &S) {
+        for (int i = 0; i < HASH_NUM; i++) {
+            rhs.push_back(RollingHash(S, bases[i]));
+        }
+    }
+    multihash_t get(int l, int r) {
+        multihash_t ret;
+        for (int i = 0; i < HASH_NUM; i++) ret[i] = rhs[i].get(l, r);
+        return ret;
+    }
+    void concat(const string &T) {
+        for (int i = 0; i < HASH_NUM; i++) rhs[i].concat(T);
+    }
 };
