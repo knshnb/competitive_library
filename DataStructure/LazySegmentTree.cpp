fa304c53ee59435f4adf20491128f23e6cd1de5c
Kenshin Abe <abe.kenshin@gmail.com>
2019-10-06 15:14:26 +0900

    Don't use std::function in LazySegmentTree

--- DataStructure/LazySegmentTree.cpp
+++ DataStructure/LazySegmentTree.cpp
@@ -1,7 +1,7 @@
 // T0: 元の配列のモノイド
 // T1: T0に対する作用素モノイド
 template <class T0, class T1>
-class SegmentTree {
+class BaseSegmentTree {
     // k番目のノードにのlazyを伝搬
     void eval(int k, int len) {
         // 定数倍高速化
@@ -44,27 +44,32 @@ public:
     vector<T0> node;
     vector<T1> lazy;
     // T0上の演算、単位元
-    using F0 = function<T0(T0, T0)>;
-    F0 f0;
-    T0 u0;
+    virtual T0 f0(T0, T0) = 0;
+    const T0 u0;
     // T1上の演算、単位元
-    using F1 = function<T1(T1, T1)>;
-    F1 f1;
-    T1 u1;
-    // 作用
-    using G = function<T0(T0, T1)>;
-    G g;
+    virtual T1 f1(T1, T1) = 0;
+    const T1 u1;
+    // T0に対するT1の作用
+    virtual T0 g(T0, T1) = 0;
     // 多数のt1(T1)に対するf1の合成
-    using P = function<T1(T1, int)>;
-    P p;
-    SegmentTree(const vector<T0>& a, F0 f0, T0 u0, F1 f1, T1 u1, G g, P p)
-        : sz(a.size()), f0(f0), u0(u0), f1(f1), u1(u1), g(g), p(p) {
+    virtual T1 p(T1, int) = 0;
+
+    BaseSegmentTree(T0 u0, T1 u1) : u0(u0), u1(u1) {}
+    void set_by_vector(const vector<T0>& a) {
+        sz = a.size();
         for (N = 1; N < sz; N *= 2)
             ;
-        node.resize(2 * N - 1);
+        node.resize(2 * N - 1, u0);
         lazy.resize(2 * N - 1, u1);
         for (int i = 0; i < sz; i++) node[N - 1 + i] = a[i];
         for (int i = N - 2; i >= 0; i--) node[i] = f0(node[2 * i + 1], node[2 * i + 2]);
+    }
+    void set_by_unit(int n) {
+        sz = n;
+        for (N = 1; N < sz; N *= 2)
+            ;
+        node.resize(2 * N - 1, u0);
+        lazy.resize(2 * N - 1, u1);
     }
     // [a, b)にxを作用
     void update(int a, int b, T1 x) {
@@ -77,56 +82,86 @@ public:
     T0 query(int a) { return query(a, a + 1); }
 };
 
-signed main() {
-    // // Min & Update
-    // SegmentTree<int, int> seg(
-    //   vector<int>(n, (1LL << 31) - 1),
-    //   [](int x, int y) { return min(x, y); }, 1e18,
-    //   [](int x, int y) { return y == 1e18 ? x : y; }, 1e18,
-    //   [](int x, int y) { return y == 1e18 ? x : y; },
-    //   [](int y, int len) { return y; }
-    // );
+template <class T0, class T1>
+struct MinUpdateQuery : public BaseSegmentTree<T0, T1> {
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    static constexpr T0 _u0 = numeric_limits<T0>::max();
+    static constexpr T1 _u1 = numeric_limits<T1>::min();
+    MinUpdateQuery() : MinUpdateQuery(_u0, _u1) {}
+    T0 f0(T0 x, T0 y) override { return min(x, y); }
+    T1 f1(T1 x, T1 y) override { return y == _u1 ? x : y; }
+    T0 g(T0 x, T1 y) override { return y == _u1 ? x : y; }
+    T1 p(T1 x, int len) override { return x; }
+};
 
-    // // Sum & Add
-    // SegmentTree<int, int> seg(
-    //   vector<int>(n, 0),
-    //   plus<int>(), 0,
-    //   plus<int>(), 0,
-    //   plus<int>(),
-    //   multiplies<int>()
-    // );
+template <class T0, class T1>
+struct SumAddQuery : public BaseSegmentTree<T0, T1> {
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    SumAddQuery() : SumAddQuery(0, 0) {}
+    T0 f0(T0 x, T0 y) override { return x + y; }
+    T1 f1(T1 x, T1 y) override { return x + y; }
+    T0 g(T0 x, T1 y) override { return x + y; }
+    T1 p(T1 x, int len) override { return x * len; }
+};
 
-    // // Min & Add
-    // SegmentTree<int, int> seg(
-    //   vector<int>(n, 0),
-    //   [](int x, int y) { return min(x, y); }, 1e18,
-    //   plus<int>(), 0,
-    //   plus<int>(),
-    //   [](int y, int len) { return y; }
-    // );
+template <class T0, class T1>
+struct MinAddQuery : public BaseSegmentTree<T0, T1> {
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    static constexpr int _u0 = numeric_limits<T0>::max();
+    MinAddQuery() : MinAddQuery(_u0, 0) {}
+    T0 f0(T0 x, T0 y) override { return min(x, y); }
+    T1 f1(T1 x, T1 y) override { return x + y; }
+    T0 g(T0 x, T1 y) override { return x + y; }
+    T1 p(T1 x, int len) override { return x; }
+};
 
-    // // Sum & Update
-    // constexpr int u1 = 1e18;
-    // SegmentTree<int, int> seg(
-    //   vector<int>(n),
-    //   plus<int>(), 0,
-    //   [](int x, int y) { return y == u1 ? x : y; }, u1,
-    //   [](int x, int y) { return y == u1 ? x : y; },
-    //   [](int y, int len) { return y == u1 ? u1 : y * len; }
-    // );
+template <class T0, class T1>
+struct SumUpdateQuery : public BaseSegmentTree<T0, T1> {
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    static constexpr int _u1 = numeric_limits<T1>::min();
+    SumUpdateQuery() : SumUpdateQuery(0, _u1) {}
+    T0 f0(T0 x, T0 y) override { return x + y; }
+    T1 f1(T1 x, T1 y) override { return y == _u1 ? x : y; }
+    T0 g(T0 x, T1 y) override { return y == _u1 ? x : y; }
+    T1 p(T1 x, int len) override { return x == _u1 ? _u1 : x * len; }
+};
 
-    // // Sum & Affine
-    // using T = mint;
-    // using Affine = pair<T, T>;
-    // SegmentTree<Affine, Affine> seg(
-    //   vector<Affine>(n),
-    //   [](Affine x, Affine y) { return Affine(x.first + y.first, x.second + y.second); }, Affine(0, 0),
-    //   [](Affine x, Affine y) { return Affine(x.first * y.first, x.second * y.first + y.second); }, Affine(1, 0),
-    //   [](Affine x, Affine y) { return Affine(x.first * y.first, x.second * y.first + y.second); },
-    //   [](Affine y, int len) { return Affine(y.first, y.second * len); }
-    // );
-    // // seg.update(i, j, {a, b}); // [i, j)にax + bを作用
-    // // seg.update(i, j, {0, a}); // update
-    // // seg.update(i, j, {1, a}); // 加算
-    // // seg.update(i, j, {a, 0}); // 倍
-}
+template <class T0>
+struct SumAffineQuery : public BaseSegmentTree<T0, pair<T0, T0>> {
+    using T1 = pair<T0, T0>; // first * x + second
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    static constexpr T1 _u1 = {1, 0};
+    SumAffineQuery() : SumAffineQuery(0, {1, 0}) {}  // TODO: _u1を使うとコンパイル通らない原因不明
+    T0 f0(T0 x, T0 y) override { return x + y; }
+    T1 f1(T1 x, T1 y) override { return {x.first * y.first, x.second * y.first + y.second}; }
+    T0 g(T0 x, T1 y) override { return y.first * x + y.second; }
+    T1 p(T1 x, int len) override { return {x.first, x.second * len}; }
+    // update(i, j, {a, b}); // [i, j)にax + bを作用
+    // update(i, j, {0, a}); // update
+    // update(i, j, {1, a}); // 加算
+    // update(i, j, {a, 0}); // 倍
+};
+
+template <class T>
+struct MinmaxAffineQuery : public BaseSegmentTree<pair<T, T>, pair<T, T>> {
+    using T0 = pair<T, T>; // {min, max}
+    using T1 = pair<T, T>; // first * x + second
+    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
+    static constexpr T0 get_u0() {
+        return {numeric_limits<T>::max(), -numeric_limits<T>::max()};
+    }
+    static constexpr T1 _u1 = {1, 0};
+    MinmaxAffineQuery() : MinmaxAffineQuery(get_u0(), {1, 0}) {}  // TODO: _u1を使うとコンパイル通らない原因不明
+    T0 f0(T0 x, T0 y) override { return {min(x.first, y.first), max(x.second, y.second)}; }
+    T1 f1(T1 x, T1 y) override { return {x.first * y.first, x.second * y.first + y.second}; }
+    T0 g(T0 x, T1 y) override {
+        T0 ret = {x.first * y.first + y.second, x.second * y.first + y.second};
+        if (y.first < 0) swap(ret.first, ret.second);
+        return ret;
+    }
+    T1 p(T1 x, int len) override { return x; }
+    // update(i, j, {a, b}); // [i, j)にax + bを作用
+    // update(i, j, {0, a}); // update
+    // update(i, j, {1, a}); // 加算
+    // update(i, j, {a, 0}); // 倍
+};
