1539334c33de9239bf4ced2823a4850602118884
Kenshin Abe <abe.kenshin@gmail.com>
2019-11-07 21:17:32 +0900

    Non-recursive SegmentTree

--- src/DataStructure/SegmentTree.cpp
+++ src/DataStructure/SegmentTree.cpp
@@ -1,55 +1,38 @@
-template <class T = int>
-class SegTree {
-    using VT = vector<T>;
-    int orig_n;
-    // k番目のノードの[l, r)について[a, b)を求める
-    T query(int a, int b, int k, int l, int r) {
-        if (r <= a || b <= l) {
-            return UNIT;
+template <class T, class F>
+struct SegmentTree {
+    const F op;
+    const T e;
+    SegmentTree(F op_, T e_) : op(op_), e(e_) {}
+    int n;
+    vector<T> t;
+    void set_by_unit(int n_) {
+        t.clear();
+        n = n_;
+        t.resize(2 * n, e);
+    }
+    void set_by_vector(const vector<T>& a) {
+        t.clear();
+        n = a.size();
+        t.resize(2 * n, e);
+        for (int i = 0; i < n; i++) t[i + n] = a[i];
+        build();
+    }
+    void build() { for (int i = n - 1; i; --i) t[i] = op(t[2 * i], t[2 * i + 1]); }
+    T& operator[](int i) { return t[i + n]; }
+    // [l, r)
+    T query(int l, int r) const {
+        assert(0 <= l && l <= r && r <= n);
+        T resl = e, resr = e;
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) resl = op(resl, t[l++]);
+            if (r & 1) resr = op(t[--r], resr);
         }
-        if (a <= l && r <= b) {
-            return dat[k];
-        }
-        T vl = query(a, b, k * 2 + 1, l, (l + r) / 2);
-        T vr = query(a, b, k * 2 + 2, (l + r) / 2, r);
-        return f(vl, vr);
+        return op(resl, resr);
     }
-
-public:
-    int N;
-    VT dat;
-    function<T(T, T)> f;
-    T UNIT;
-    SegTree() {}
-    SegTree(int n, function<T(T, T)> f_, const T unit) : orig_n(n), f(f_), UNIT(unit) {
-        for (N = 1; N < n; N *= 2)
-            ;
-        dat = VT(2 * N - 1, UNIT);
+    // iをaに変更
+    void update(int i, const T& a) {
+        assert(0 <= i && i < n);
+        t[i += n] = a;
+        while (i >>= 1) t[i] = op(t[2 * i], t[2 * i + 1]);
     }
-    SegTree(
-        const VT& a, function<T(T, T)> f_ = [](int a, int b) { return min(a, b); }, T unit = 1e15)
-        : orig_n(a.size()), f(f_), UNIT(unit) {
-        for (N = 1; N < orig_n; N *= 2)
-            ;
-        dat = VT(2 * N - 1);
-        REP(i, a.size()) { dat[N - 1 + i] = a[i]; }
-        for (int k = N - 2; k >= 0; k--) {
-            dat[k] = f(dat[2 * k + 1], dat[2 * k + 2]);
-        }
-    }
-    // k番目をaに
-    void update(int k, T a) {
-        k += N - 1;
-        dat[k] = a;
-        while (k > 0) {
-            k = (k - 1) / 2;
-            dat[k] = f(dat[2 * k + 1], dat[2 * k + 2]);
-        }
-    }
-    // [a, b)でのクエリ
-    T query(int a, int b) {
-        assert(0 <= a && a < b && b <= orig_n);
-        return query(a, b, 0, 0, N);
-    }
-    T query(int a) { return query(a, a + 1); }
 };
