5810a8ba3e8f2eb68ed211b3ae0101c7b16545b3
Kenshin Abe <abe.kenshin@gmail.com>
2019-12-23 15:39:22 +0900

    Use int instead of Int in TreeInput

--- src/Helper/TreeInput.cpp
+++ src/Helper/TreeInput.cpp
@@ -1,7 +1,7 @@
-Int n = in();
-vector<vector<Int>> g(n);
-REP(i, n - 1) {
-    Int u = in(), v = in();
+int n = in();
+vector<vector<int>> g(n);
+for (int i = 0; i < n - 1; i++) {
+    int u = in(), v = in();
     u--, v--;
     g[u].push_back(v);
     g[v].push_back(u);
