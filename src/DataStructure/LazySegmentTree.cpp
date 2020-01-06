f7e5ffff22c0904bcec9bd67c31126e485bbf5ab
Kenshin Abe <abe.kenshin@gmail.com>
2020-01-07 01:52:00 +0900

    Use lambda instead of inheritance in LazySegmentTree

--- src/DataStructure/LazySegmentTree.cpp
+++ src/DataStructure/LazySegmentTree.cpp
@@ -1,6 +1,6 @@
 // T0: 元の配列のモノイド
 // T1: T0に対する作用素モノイド
-template <class T0, class T1> class BaseSegmentTree {
+template <class T0, class T1, class F0, class F1, class G, class P> class LazySegmentTree {
     // k番目のノードにのlazyを伝搬
     void eval(int k, int len) {
         // 定数倍高速化
@@ -43,17 +43,17 @@ public:
     vector<T0> node;
     vector<T1> lazy;
     // T0上の演算、単位元
-    virtual T0 f0(T0, T0) = 0;
+    const F0 f0;
     const T0 u0;
     // T1上の演算、単位元
-    virtual T1 f1(T1, T1) = 0;
+    const F1 f1;
     const T1 u1;
     // T0に対するT1の作用
-    virtual T0 g(T0, T1) = 0;
+    const G g;
     // 多数のt1(T1)に対するf1の合成
-    virtual T1 p(T1, int) = 0;
+    const P p;
 
-    BaseSegmentTree(T0 u0, T1 u1) : u0(u0), u1(u1) {}
+    LazySegmentTree(F0 f0, T0 u0, F1 f1, T1 u1, G g, P p) : f0(f0), u0(u0), f1(f1), u1(u1), g(g), p(p) {}
     void set_by_vector(const vector<T0>& a) {
         sz = a.size();
         for (N = 1; N < sz; N *= 2)
@@ -79,80 +79,17 @@ public:
     // [a, b)
     T0 query(int a, int b) { return query(a, b, 0, 0, N); }
     T0 query(int a) { return query(a, a + 1); }
-};
-
-template <class T0, class T1> struct MinUpdateQuery : public BaseSegmentTree<T0, T1> {
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    static constexpr T0 _u0 = numeric_limits<T0>::max();
-    static constexpr T1 _u1 = numeric_limits<T1>::min();
-    MinUpdateQuery() : MinUpdateQuery(_u0, _u1) {}
-    T0 f0(T0 x, T0 y) override { return min(x, y); }
-    T1 f1(T1 x, T1 y) override { return y == _u1 ? x : y; }
-    T0 g(T0 x, T1 y) override { return y == _u1 ? x : y; }
-    T1 p(T1 x, int len) override { return x; }
-};
-
-template <class T0, class T1> struct SumAddQuery : public BaseSegmentTree<T0, T1> {
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    SumAddQuery() : SumAddQuery(0, 0) {}
-    T0 f0(T0 x, T0 y) override { return x + y; }
-    T1 f1(T1 x, T1 y) override { return x + y; }
-    T0 g(T0 x, T1 y) override { return x + y; }
-    T1 p(T1 x, int len) override { return x * len; }
-};
-
-template <class T0, class T1> struct MinAddQuery : public BaseSegmentTree<T0, T1> {
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    static constexpr int _u0 = numeric_limits<T0>::max();
-    MinAddQuery() : MinAddQuery(_u0, 0) {}
-    T0 f0(T0 x, T0 y) override { return min(x, y); }
-    T1 f1(T1 x, T1 y) override { return x + y; }
-    T0 g(T0 x, T1 y) override { return x + y; }
-    T1 p(T1 x, int len) override { return x; }
-};
-
-template <class T0, class T1> struct SumUpdateQuery : public BaseSegmentTree<T0, T1> {
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    static constexpr int _u1 = numeric_limits<T1>::min();
-    SumUpdateQuery() : SumUpdateQuery(0, _u1) {}
-    T0 f0(T0 x, T0 y) override { return x + y; }
-    T1 f1(T1 x, T1 y) override { return y == _u1 ? x : y; }
-    T0 g(T0 x, T1 y) override { return y == _u1 ? x : y; }
-    T1 p(T1 x, int len) override { return x == _u1 ? _u1 : x * len; }
-};
-
-template <class T0> struct SumAffineQuery : public BaseSegmentTree<T0, pair<T0, T0>> {
-    using T1 = pair<T0, T0>;  // first * x + second
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    static constexpr T1 _u1 = {1, 0};
-    SumAffineQuery() : SumAffineQuery(0, {1, 0}) {}  // TODO: _u1を使うとコンパイル通らない原因不明
-    T0 f0(T0 x, T0 y) override { return x + y; }
-    T1 f1(T1 x, T1 y) override { return {x.first * y.first, x.second * y.first + y.second}; }
-    T0 g(T0 x, T1 y) override { return y.first * x + y.second; }
-    T1 p(T1 x, int len) override { return {x.first, x.second * len}; }
-    // update(i, j, {a, b}); // [i, j)にax + bを作用
-    // update(i, j, {0, a}); // update
-    // update(i, j, {1, a}); // 加算
-    // update(i, j, {a, 0}); // 倍
-};
-
-template <class T> struct MinmaxAffineQuery : public BaseSegmentTree<pair<T, T>, pair<T, T>> {
-    using T0 = pair<T, T>;  // {min, max}
-    using T1 = pair<T, T>;  // first * x + second
-    using BaseSegmentTree<T0, T1>::BaseSegmentTree;
-    static constexpr T0 get_u0() { return {numeric_limits<T>::max(), -numeric_limits<T>::max()}; }
-    static constexpr T1 _u1 = {1, 0};
-    MinmaxAffineQuery() : MinmaxAffineQuery(get_u0(), {1, 0}) {}  // TODO: _u1を使うとコンパイル通らない原因不明
-    T0 f0(T0 x, T0 y) override { return {min(x.first, y.first), max(x.second, y.second)}; }
-    T1 f1(T1 x, T1 y) override { return {x.first * y.first, x.second * y.first + y.second}; }
-    T0 g(T0 x, T1 y) override {
-        T0 ret = {x.first * y.first + y.second, x.second * y.first + y.second};
-        if (y.first < 0) swap(ret.first, ret.second);
-        return ret;
+    friend string to_string(LazySegmentTree<T0, T1, F0, F1, G, P>& seg) {
+        for (int i = 0; i < seg.sz; i++) seg.query(i);
+        return to_string(vector<T0>(seg.node.begin() + (seg.N - 1), seg.node.begin() + (seg.N - 1 + seg.sz)));
     }
-    T1 p(T1 x, int len) override { return x; }
-    // update(i, j, {a, b}); // [i, j)にax + bを作用
-    // update(i, j, {0, a}); // update
-    // update(i, j, {1, a}); // 加算
-    // update(i, j, {a, 0}); // 倍
 };
+template <class T0, class T1, class F0, class F1, class G, class P>
+auto make_lazy_segment_tree(F0 f0, T0 u0, F1 f1, T1 u1, G g, P p) {
+    return LazySegmentTree<T0, T1, F0, F1, G, P>(f0, u0, f1, u1, g, p);
+}
+// Max && Add
+// constexpr Int INF = 1e18;
+// auto seg = make_lazy_segment_tree<Int, Int>(
+//     [](Int x, Int y) { return max(x, y); }, -INF, [](Int x, Int y) { return x + y; }, 0,
+//     [](Int x, Int y) { return x == -INF ? x : x + y; }, [](Int x, int len) { return x; });
