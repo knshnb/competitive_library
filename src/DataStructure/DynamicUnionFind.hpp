5ab9e5bd2e46a28965e392f1344d07f55cb56a6e
Kenshin Abe <abe.kenshin@gmail.com>
2020-04-25 17:49:02 +0900

    DynamicUnionFind

--- src/DataStructure/DynamicUnionFind.hpp
+++ src/DataStructure/DynamicUnionFind.hpp
@@ -1,30 +1,18 @@
 struct DynamicUnionFind {
-    unordered_map<int, int> number;
-    // 頂点xが入っていなかったら追加
-    struct Data {
-        Data() {}
-        void merge(Data& s) {
-            // 自分で定義
-        }
-    };
-    unordered_map<int, Data> data;
+    std::unordered_map<int, int> number;
 
     int root(int x) {
         if (!number.count(x)) number[x] = -1;
         return number[x] < 0 ? x : number[x] = root(number[x]);
     }
     void unite(int x, int y) {
-        x = root(x);
-        y = root(y);
+        x = root(x), y = root(y);
         if (x == y) return;
-        if (number[y] < number[x]) swap(x, y);
+        if (number[y] < number[x]) std::swap(x, y);
         // yをxにマージ
         number[x] += number[y];
         number[y] = x;
-        // dataのマージ方法に合わせて変える
-        data[x].merge(data[y]);
     }
     bool is_same(int x, int y) { return root(x) == root(y); }
     int size(int x) { return -number[root(x)]; }
-    Data& ref(int x) { return data[root(x)]; }
 };
---
+++ test/yosupo/unionfind_dynamic.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/unionfind"
+
+#include <bits/stdc++.h>  // clang-format off
+using Int = long long;
+#define REP_(i, a_, b_, a, b, ...) for (Int i = (a), lim##i = (b); i < lim##i; i++)
+#define REP(i, ...) REP_(i, __VA_ARGS__, __VA_ARGS__, 0, __VA_ARGS__)
+struct SetupIO { SetupIO() { std::cin.tie(nullptr), std::ios::sync_with_stdio(false), std::cout << std::fixed << std::setprecision(13); } } setup_io;
+#ifndef _MY_DEBUG
+#define dump(...)
+#endif  // clang-format on
+
+/**
+ *    author:  knshnb
+ *    created: Sun Mar 29 05:04:23 JST 2020
+ **/
+
+#define CALL_FROM_TEST
+#include "../../src/DataStructure/DynamicUnionFind.hpp"
+#undef CALL_FROM_TEST
+
+signed main() {
+    Int n, Q;
+    std::cin >> n >> Q;
+    DynamicUnionFind uf;
+    REP(q, Q) {
+        Int t, u, v;
+        std::cin >> t >> u >> v;
+        if (t == 0) {
+            uf.unite(u, v);
+        } else {
+            std::cout << uf.is_same(u, v) << "\n";
+        }
+    }
+}
