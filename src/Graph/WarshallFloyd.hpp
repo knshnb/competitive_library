499564deb141999927fdaa46f14ef9b8578f5ca1
Kenshin Abe <abe.kenshin@gmail.com>
2020-04-28 00:07:31 +0900

    Arrange

--- src/Graph/WarshallFloyd.hpp
+++ src/Graph/WarshallFloyd.hpp
@@ -1,10 +1,10 @@
-template <class T> void warshall_floyd(vector<vector<T>> &d) {
+template <class T> void warshall_floyd(std::vector<std::vector<T>> &d) {
     int n = d.size();
     for (int i = 0; i < n; i++) assert(d[i][i] == 0);
     for (int k = 0; k < n; k++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                d[i][j] = std::min(d[i][j], d[i][k] + d[k][j]);
             }
         }
     }
