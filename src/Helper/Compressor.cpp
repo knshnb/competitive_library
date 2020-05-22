41a9728517757b5a828b68ff0e1466294dbd2acd
Kenshin Abe <abe.kenshin@gmail.com>
2020-05-22 22:25:40 +0900

    Compressor

--- src/Helper/Compressor.cpp
+++ src/Helper/Compressor.cpp
@@ -1,10 +1,12 @@
 template <class T> struct Compressor {
     std::vector<T> val;
     void insert(T x) { val.push_back(x); }
+    void insert(const std::vector<T>& v) { val.insert(val.begin(), v.begin(), v.end()); }
     void build() {
         std::sort(val.begin(), val.end());
         val.erase(std::unique(val.begin(), val.end()), val.end());
     }
-    int operator[](T x) { return std::lower_bound(val.begin(), val.end(), x) - val.begin(); }
+    int operator()(T x) { return std::lower_bound(val.begin(), val.end(), x) - val.begin(); }
+    T operator[](int idx) { return val[idx]; }
     int size() { return val.size(); }
 };
