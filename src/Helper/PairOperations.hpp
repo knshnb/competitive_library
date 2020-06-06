6aad85c962873dcb2af6b6f5bc54ea690983fc95
Kenshin Abe <abe.kenshin@gmail.com>
2020-06-06 22:51:08 +0900

    Pair Operations

--- src/Helper/PairOperations.hpp
+++ src/Helper/PairOperations.hpp
@@ -5,3 +5,6 @@ std::pair<A, B>& operator+=(std::pair<A, B>& x, const std::pair<C, D>& y) {
 template <class A, class B, class C, class D> std::pair<A, B> operator+(std::pair<A, B> x, const std::pair<C, D>& y) {
     return x += y;
 }
+template <class T, class integer> T& get(std::vector<std::vector<T>>& v, const std::pair<integer, integer>& p) {
+    return v[p.first][p.second];
+}
