3cc27d500d1d2a62f1bed886ea17fd7a166ee251
Kenshin Abe <abe.kenshin@gmail.com>
2020-08-29 03:59:22 +0900

    Update ModInt

--- src/Math/ModInt.hpp
+++ src/Math/ModInt.hpp
@@ -12,13 +12,16 @@ template <class T> T pow(T x, long long n, const T UNION = 1) {
 template <int Mod> struct ModInt {
     int x;
     static int runtime_mod;
-    static std::unordered_map<int, int> to_inv;
     // テンプレート引数が負のときは実行時ModInt
     static int mod() { return Mod < 0 ? runtime_mod : Mod; }
+    static std::unordered_map<int, int>& to_inv() {
+        static std::unordered_map<int, int> to_inv_;
+        return to_inv_;
+    }
     static void set_runtime_mod(int mod) {
         static_assert(Mod < 0, "template parameter Mod must be negative for runtime ModInt");
         runtime_mod = mod;
-        to_inv.clear();
+        to_inv().clear();
     }
     ModInt() : x(0) {}
     ModInt(long long x_) {
@@ -38,8 +41,8 @@ template <int Mod> struct ModInt {
         return *this;
     }
     ModInt& operator/=(ModInt rhs) {
-        x = (unsigned long long)x * rhs.inv().x % mod();
-        return *this;
+        ModInt inv = to_inv().count(rhs.x) ? to_inv()[rhs.x] : (to_inv()[rhs.x] = pow(rhs, mod() - 2).x);
+        return *this *= inv;
     }
     ModInt operator-() const { return -x < 0 ? mod() - x : -x; }
     ModInt operator+(ModInt rhs) const { return ModInt(*this) += rhs; }
@@ -48,7 +51,6 @@ template <int Mod> struct ModInt {
     ModInt operator/(ModInt rhs) const { return ModInt(*this) /= rhs; }
     bool operator==(ModInt rhs) const { return x == rhs.x; }
     bool operator!=(ModInt rhs) const { return x != rhs.x; }
-    ModInt inv() const { return to_inv.count(this->x) ? to_inv[this->x] : (to_inv[this->x] = pow(*this, mod() - 2).x); }
 
     friend std::ostream& operator<<(std::ostream& s, ModInt<Mod> a) {
         s << a.x;
@@ -62,7 +64,6 @@ template <int Mod> struct ModInt {
     }
     friend std::string to_string(ModInt<Mod> a) { return std::to_string(a.x); }
 };
-template <int Mod> std::unordered_map<int, int> ModInt<Mod>::to_inv;
 template <int Mod> int ModInt<Mod>::runtime_mod;
 
 #ifndef CALL_FROM_TEST
