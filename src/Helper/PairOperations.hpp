215c856987f84d3fcdb79cf14986ed772e9bf5e3
Kenshin Abe <abe.kenshin@gmail.com>
2021-03-05 23:10:45 +0900

    Add - to PairOperations

--- src/Helper/PairOperations.hpp
+++ src/Helper/PairOperations.hpp
@@ -5,6 +5,13 @@ std::pair<A, B>& operator+=(std::pair<A, B>& x, const std::pair<C, D>& y) {
 template <class A, class B, class C, class D> std::pair<A, B> operator+(std::pair<A, B> x, const std::pair<C, D>& y) {
     return x += y;
 }
+template <class A, class B, class C, class D>
+std::pair<A, B>& operator-=(std::pair<A, B>& x, const std::pair<C, D>& y) {
+    return x = {x.first - y.first, x.second - y.second};
+}
+template <class A, class B, class C, class D> std::pair<A, B> operator-(std::pair<A, B> x, const std::pair<C, D>& y) {
+    return x -= y;
+}
 template <class T, class integer> T& get(std::vector<std::vector<T>>& v, const std::pair<integer, integer>& p) {
     return v[p.first][p.second];
 }
